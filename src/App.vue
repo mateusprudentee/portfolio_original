8cf2b4f61fcef24ac0ee9f3d7b7a8f22d5e9feea
Mateus <mateus19102005@gmail.com>
2025-03-23 15:11:45 -0400

    Atualizando arquivos do portfólio

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,654 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="espaco-dois"></div>
+  <div id="app">
+    <div v-if="loading" class="loader-overlay">
+      <div class="loader"></div>
+    </div>
+  </div>
+
+  <div class="container">
+    <div class="header-info" id="header">
+      <div class="header-info-img"></div>
+      <div class="header-info-title">
+        <p>Mateus Prudente</p>
+      </div>
+      <div class="header-info-subtitle" id="profissao">
+        <p>Engenheiro de Software</p>
+      </div>
+      <div class="atualmente">
+        <p>Atualmente como Assistente de Suporte na Unimed há mais de 1 ano (desde abril 2024)</p>
+      </div>
+      <div class="flex-icons" id="icons">
+
+      <div class="linkedin">
+      <a href="https://www.linkedin.com/in/mateusprudentee/" target="_blank" style="color: inherit; text-decoration: none;">
+      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor">
+        <path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.761 0 5-2.239 5-5v-14c0-2.761-2.239-5-5-5zm-11 19h-3v-10h3v10zm-1.5-11.25c-.966 0-1.75-.784-1.75-1.75s.784-1.75 1.75-1.75 1.75.784 1.75 1.75-.784 1.75-1.75 1.75zm13.5 11.25h-3v-5.5c0-1.104-.896-2-2-2s-2 .896-2 2v5.5h-3v-10h3v1.179c.646-.891 1.645-1.179 2.5-1.179 2.208 0 4 1.792 4 4v6z"/>
+      </svg>
+      </a>
+      </div>
+
+      <div class="whats">
+      <div class="whatsapp-icon">
+        <a href="https://wa.me/5565993622978" target="_blank" style="text-decoration: none; color: inherit;">
+          <img src="https://upload.wikimedia.org/wikipedia/commons/6/6b/WhatsApp.svg" alt="WhatsApp" style="width: 24px; height: 24px;">
+        </a>
+      </div>
+      </div>
+
+
+
+      </div>
+    </div>
+    <div class="text-all" id="text-all">
+      <div class="espaco"></div>
+      <p>
+        Minha motivação e interesse profundo na área de software e desenvolvimento iniciou em 2021. Nos anos de 2021 a 2022 tive o foco principalmente na ára de Desenvolvimento Web e Banco de Dados (MySQL & Oracle), ficando nos anos de 2023 e 2024 para focado no Java, JavaScript e Ruby.
+        <br><br>
+        Em 2024 <a href="https://www.unimed.coop.br/site/web/nortedomatogrosso" style="color: white; text-decoration: none;">iniciei minha carreira profissional na Unimed Norte do Mato Grosso</a>  tendo a oportunidade de desempenhar meus conhecimentos técnicos e em um ambiente colaborativo juntamente em uma equipe de 14 pessoas, no núcleo de Gestão da Informação e Negócios.
+      </p>
+      <div class="espaco-dois"></div>
+      <div class="text-all-title">
+        <p>Experiência (1)</p>
+      </div>
+      <div class="linha"></div>
+      <div class="experiencia-um">
+        <div class="item-um">
+          <div class="text-all-subtitle">
+            <p>Abril - Atualmente</p>
+          </div>
+          <div class="text-unimed">
+            <p>Assistente de Suporte</p>
+          </div>
+          <div class="text-unimed-name">
+            <p>Unimed Norte do Mato Grosso</p>
+          </div>
+          <div class="text-unimed-text">
+            <p>Responsável por atividades que envolve o desenvolvimento, gestão de projetos, documentação e tudo que abrange o 
+              núcleo de Gestão da Informação e Negócios.<br><br>O núcleo de Gestão da Informação e Negócio prioriza a 
+              inteligência dos sistemas de software internos, bem como prezar pelos principios de integridade, confidencialidade, 
+              disponibilidade e autenticidade da operadora.</p>
+              <div class="badges">
+
+                <p style="background: #00ff1640;
+                font-weight: bold;
+                color: #02ff00;
+                border-radius: 9999px; font-size: 10px; width: 55px; text-align: center; padding: 0.3em;">HTML</p>
+
+                <p style="margin-left: 5px; background: #00ff1640;
+                font-weight: bold;
+                color: #02ff00;
+                border-radius: 9999px; font-size: 10px; width: 55px; text-align: center; padding: 0.3em;">CSS</p>
+
+                <p style="margin-left: 5px; background: #00ff1640;
+                font-weight: bold;
+                color: #02ff00;
+                border-radius: 9999px; font-size: 10px; width: 55px; text-align: center; padding: 0.3em;">VUE</p>
+
+                <p style="margin-left: 5px; background: #00ff1640;
+                font-weight: bold;
+                color: #02ff00;
+                border-radius: 9999px; font-size: 10px; width: 60px; text-align: center; padding: 0.3em;">QUASAR</p>
+
+                <p style="margin-left: 5px; background: #00ff1640;
+                font-weight: bold;
+                color: #02ff00;
+                border-radius: 9999px; font-size: 10px; width: 80px; text-align: center; padding: 0.3em;">SELENIUM</p>
+
+              </div>
+              <div class="badges-two">
+
+                <p style="background: #00ff1640;
+                font-weight: bold;
+                color: #02ff00;
+                border-radius: 9999px; font-size: 10px; width: 55px; text-align: center; padding: 0.3em;">JAVA</p>
+
+                <p style="margin-left: 5px; background: #00ff1640;
+                font-weight: bold;
+                color: #02ff00;
+                border-radius: 9999px; font-size: 10px; width: 35px; text-align: center; padding: 0.3em;">JS</p>
+
+                <p style="margin-left: 5px; background: #00ff1640;
+                font-weight: bold;
+                color: #02ff00;
+                border-radius: 9999px; font-size: 10px; width: 55px; text-align: center; padding: 0.3em;">BIZAGI</p>
+
+                <p style="margin-left: 5px; background: #00ff1640;
+                font-weight: bold;
+                color: #02ff00;
+                border-radius: 9999px; font-size: 10px; width: 60px; text-align: center; padding: 0.3em;">PYTHON</p>
+
+                <p style="margin-left: 5px; background: #00ff1640;
+                font-weight: bold;
+                color: #02ff00;
+                border-radius: 9999px; font-size: 10px; width: 50px; text-align: center; padding: 0.3em;">FLASK</p>
+
+                </div>
+          </div>
+        </div>
+      <div class="espaco-dois"></div>
+      <div class="text-all-title">
+        <p>Formação (3)</p>
+      </div>
+      <div class="linha"></div>
+      <div class="formacao-um">
+          <div class="text-all-subtitle">
+            <p>2025 - 2026</p>
+          </div>
+          <div class="text-software">
+            <p>Pós Graduação em Inteligência Artificial</p>
+          </div>
+          <div class="text-software-name">
+            <p>Faculdade Líbano</p>
+          </div>
+          <div class="text-unimed-text">
+            <p>
+              — Aprendizagem de Máquinas
+              <br>
+              — Legislação e Copyright
+              <br>
+              — Crimes Cibernéticos
+              <br>
+              — Linguagem Natural
+              <br>
+              — Linguagem Neural
+              <br>
+              — Algoritmos não supervisionados
+              <br>
+              — Algoritmos supervisionados
+            </p>
+          </div>
+          </div>
+      <div class="formacao-um">
+          <div class="text-all-subtitle">
+            <p>2025 - 2026</p>
+          </div>
+          <div class="text-software">
+            <p>Pós Graduação em Engenharia de Software</p>
+          </div>
+          <div class="text-software-name">
+            <p>Faculdade Líbano</p>
+          </div>
+          <div class="text-unimed-text">
+            <p>
+              — Modelagem de Requisitos
+              <br>
+              — Qualidade de Software
+              <br>
+              — Desenvolvimento Acessível
+              <br>
+              — Normas de Qualidade de Software
+              <br>
+              — Fundamentos de Testes de Software
+              <br>
+              — Metodologias Ágeis
+              <br>
+              — Python, JavaScript e Java
+            </p>
+          </div>
+          </div>
+          <div class="formacao-um">
+          <div class="text-all-subtitle">
+            <p>2024 - 2025</p>
+          </div>
+          <div class="text-software">
+            <p>Análise e Desenvolvimento de Sistemas</p>
+          </div>
+          <div class="text-software-name">
+            <p>ETEP - Centro Universitário</p>
+          </div>
+          <div class="text-unimed-text">
+            <p>
+              — Desenvolvimento Web
+              <br>
+              — Desenvolvimento Mobile
+              <br>
+              — Desenvolvimento Acessível
+              <br>
+              — Gestão de Projetos
+              <br>
+              — Inteligência Artificial
+              <br>
+              — Documentação técnica
+              <br>
+              — Estratégias técnicas
+            </p>
+          </div>
+          </div>
+      </div>
+      <div class="footer">Portfólio desenvolvido por Mateus e inspirado em  <a href="https://brittanychiang.com/" style="text-decoration: none; color: gray;">Brittany Chiang site</a>.</div>
+    </div>
+    </div>
+    <HelloWorld />
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<style>
+ .tooltip {
+            position: relative;
+            display: inline-block;
+            color: inherit;
+            text-decoration: none;
+        }
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+        /* Estilo do tooltip */
+        .tooltip .tooltip-text {
+            visibility: hidden;
+            width: 140px;
+            background-color: black;
+            color: #fff;
+            text-align: center;
+            border-radius: 4px;
+            padding: 5px;
+            position: absolute;
+            z-index: 1;
+            bottom: 125%; /* Posiciona acima do ícone */
+            left: 50%;
+            margin-left: -70px; /* Centraliza o tooltip */
+            opacity: 0; /* Inicialmente invisível */
+            transition: opacity 0.3s; /* Animação de transição */
+        }
+
+        /* Efeito de hover */
+        .tooltip:hover .tooltip-text {
+            visibility: visible;
+            opacity: 1; /* Torna o tooltip visível */
+        }
+@media (max-width: 768px) {
+  #header {
+    position: relative;
+    margin: 0 auto;
+    padding-left: 0px;
+    padding: 0em;
+  }
+  #text-all {
+    margin: 0 auto;
+    margin-top: 550px;
+  }
+  #icons {
+    margin-top: -100px;
+    text-align: center;
+  }
+  #profissao {
+    margin-top: -55px;
   }
 }
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+.new {
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  padding: 0.2em;
+  height: 100%;
+  font-size: 14px;
+  border-bottom: 2px solid rgb(0, 116, 0);
+  margin-top: -5px;
+  background: #006308;
 }
+/* Definindo fundo global */
+html, body {
+  margin: 0;
+  padding: 0;
+  background-color: #000; /* ou a cor desejada */
+  color: #fff; /* texto em branco, se necessário */
+}
+
+/* Estilos adicionais para o container */
+.container {
+  background-color: #222222; /* Defina a cor de fundo desejada */
+  min-height: 100vh; /* Garante que o container cubra toda a altura da tela */
+  padding: 20px; /* Adicione um preenchimento se necessário */
+}
+
+
+.loader-overlay {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  background-color: black;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  z-index: 1000;
+}
+
+.loader {
+  border: 8px solid #f3f3f3;
+  border-top: 8px solid #02ff00;
+  border-radius: 50%;
+  width: 50px;
+  height: 50px;
+  animation: spin 1s linear infinite;
+}
+
+@keyframes spin {
+  0% { transform: rotate(0deg); }
+  100% { transform: rotate(360deg); }
+}
+
+
+
+.formacao-um-dois:hover {
+  background: rgb(22, 22, 22);
+  border-radius: 20px;
+  transform: translateY(-2px); /* Move o item 5 pixels para cima */
+  box-shadow: 0 4px 10px rgba(0, 0, 0, 0.5); /* Sombra suave */
+}
+.footer {
+  font-size: 14px;
+  color: gray;
+  margin-top: 30px;
+}
+.discord-icon {
+  display: flex;
+  align-items: center;
+  gap: 8px;
+  color: #7289DA; /* Cor típica do Discord */
+  font-weight: bold;
+  cursor: pointer;
+}
+
+.discord-icon:hover {
+  opacity: 0.8;
+}
+.whats {
+  margin-left: 10px;
+}
+.flex-icons {
+  display: flex;
+}
+.whatsapp-icon {
+  display: flex;
+  align-items: center;
+  gap: 8px;
+  color: #25D366; /* Cor típica do WhatsApp */
+  font-weight: bold;
+  cursor: pointer;
+}
+
+.whatsapp-icon:hover {
+  opacity: 0.8;
+}
+
+.badges-two {
+  margin-top: -16px;
+  display: flex;
+}
+.badges {
+  display: flex;
+}
+.espaco-tres {
+  height: 30px;
+}
+.formacao-um:hover .text-all-subtitle,
+.formacao-um:hover .text-software {
+}
+
+.formacao-um {
+  transition: transform 0.3s ease, box-shadow 0.3s ease; 
+  padding: 0.3em;
+}
+.formacao-um-dois {
+  transition: transform 0.3s ease, box-shadow 0.3s ease; 
+  padding: 0.3em;
+}
+.formacao-um:hover {
+  background: rgb(22, 22, 22);
+  border-radius: 0px 0px 20px 20px;
+  transform: translateY(-2px); /* Move o item 5 pixels para cima */
+  box-shadow: 0 4px 10px rgba(0, 0, 0, 0.5); /* Sombra suave */
+}
+
+.item-um {
+  transition: transform 0.3s ease, box-shadow 0.3s ease; 
+  padding: 0.3em;
+}
+
+.item-um:hover {
+  background: rgb(22, 22, 22);
+  border-radius: 0px 0px 20px 20px;
+  transform: translateY(-2px); /* Move o item 5 pixels para cima */
+  box-shadow: 0 4px 10px rgba(0, 0, 0, 0.5); /* Sombra suave */
+}
+.item-um:hover .text-all-subtitle,
+.item-um:hover .text-unimed,
+.item-um:hover .text-unimed-name,
+.item-um:hover .text-unimed-text {
+}
+/* Estilos da barra de rolagem */
+::-webkit-scrollbar {
+  width: 8px; /* Largura da barra de rolagem */
+  background-color: black; /* Cor de fundo da barra */
+}
+
+::-webkit-scrollbar-thumb {
+  background-color: #888; /* Cor do polegar da barra de rolagem */
+  border-radius: 10px; /* Arredondamento do polegar */
+}
+
+::-webkit-scrollbar-thumb:hover {
+  background-color: #555; /* Cor do polegar ao passar o mouse */
+}
+
+::-webkit-scrollbar-track {
+  background: black; /* Cor do trilho da barra de rolagem */
+  border-radius: 10px; /* Arredondamento do trilho */
+}
+
+.linha {
+  width: 100%;
+  height: 1px;
+  background: #353535;
+}
+.text-jogo-name {
+  margin-top: -30px;
+  margin-left: 140px;
+}
+.text-jogo {
+  display: inline-block;
+  font-size: 16px;
+  color: white;
+  font-weight: bold;
+  margin-left: 108px;
+}
+.text-software-name {
+  margin-top: -30px;
+  margin-left: 140px;
+}
+.text-software-projeto {
+  display: inline-block;
+  font-size: 16px;
+  color: white;
+  font-weight: bold;
+  margin-left: 110px;
+}
+.text-software {
+  display: inline-block;
+  font-size: 16px;
+  color: white;
+  font-weight: bold;
+  margin-left: 65px;
+}
+.text-unimed-text {
+  margin-top: 5px;
+  font-size: 16px;
+  text-align: justify;
+  max-width: 350px;
+  margin-left: 140px;
+}
+.text-unimed-name {
+  margin-top: -30px;
+  margin-left: 140px;
+}
+.text-unimed {
+  display: inline-block;
+  font-size: 16px;
+  color: white;
+  font-weight: bold;
+  margin-left: 20px;
+}
+.text-all-subtitle {
+  display: inline-block;
+  color: white;
+  font-size: 14px;
+  font-weight: bold;
+}
+.espaco-dois {
+  height: 40px;
+}
+.text-all-title {
+  font-size: 19px;
+  display: block;
+  color: white;
+}
+.espaco {
+  height: 50px;
+}
+.text-all {
+  font-size: 15px;
+  line-height: 24px;
+  text-align: justify;
+  color: gray;
+  max-width: 520px;
+  display: inline-block;
+  margin-left: 455px;
+  top: 0;
+  position: absolute;
+}
+
+.atualmente {
+  color: gray;
+  line-height: 23px;
+  font-size: 15px;
+  max-width: 300px;
+}
+
+.header-info-img {
+  background-image: url("https://i.imgur.com/n45hQXp.jpeg");
+  background-size: contain;
+  background-repeat: no-repeat;
+  background-position: center;
+  width: 200px;
+  height: 200px;
+  border-radius: 9999px;
+}
+
+.header-info-subtitle {
+  font-size: 24px; 
+  margin-top: -20px;
+}
+
+.header-info-title {
+  font-size: 30px;
+  font-weight: bold;
+}
+
+.header-info {
+  position: fixed; /* Fixa o cabeçalho no topo */
+  top: 0; /* Posição do cabeçalho */
+  padding: 5em; /* Mantém o padding já existente */
+  display: inline-block;
+  width: 300px; /* Largura já existente */
+}
+
+body {
+  font-family: 'Ubuntu', sans-serif;
+  margin: 0; /* Remove qualquer margem padrão do body */
+}
+
+.container {
+  max-width: 970px;
+  background: black;
+  margin: 0 auto; /* Centraliza o container na página */
+  align-items: center; /* Centraliza os itens verticalmente */
+  justify-content: center; /* Centraliza os itens horizontalmente */
+  flex-direction: column; /* Opcional: organiza os itens em coluna, se necessário */
+}
+
+
+.text-all-title {
+  display: block; /* Garante que o título ocupa a largura completa */
+  clear: both; /* Impede que o conteúdo acima se sobreponha a ele */
+  margin-top: 20px; /* Adiciona um espaçamento acima, se necessário */
+}
+/* Estilos para telas pequenas */
+@media (max-width: 768px) {
+  .container {
+    padding: 1em;
+  }
+
+  .header-info {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  .header-info-img,
+  .header-info-title,
+  .header-info-subtitle,
+  .atualmente {
+    text-align: center;
+    margin: 0.5em 0;
+  }
+
+  .flex-icons {
+    justify-content: center;
+  }
+
+  .text-all {
+    padding: 0 1em;
+  }
+
+  .text-unimed-text,
+  .text-software-name,
+  .text-software,
+  .text-jogo,
+  .text-jogo-name {
+    margin-left: 0;
+    max-width: 100%;
+  }
+
+  .badges, .badges-two {
+    flex-wrap: wrap;
+    justify-content: center;
+  }
+}
+
+/* Estilos para telas médias */
+@media (min-width: 768px) and (max-width: 1024px) {
+  .text-all {
+    padding: 0 2em;
+  }
+
+  .text-unimed-text,
+  .text-software-name,
+  .text-software,
+  .text-jogo,
+  .text-jogo-name {
+    margin-left: 1em;
+  }
+}
+
 </style>
+
+<script>
+export default {
+  data() {
+    return {
+      loading: true, // Inicializa o estado de carregamento como verdadeiro
+    };
+  },
+  mounted() {
+    // Adiciona um evento para escutar quando a página estiver completamente carregada
+    window.addEventListener('load', this.onLoad);
+  },
+  methods: {
+    onLoad() {
+      this.loading = false; // Altera o estado de carregamento para falso
+    },
+  },
+  beforeUnmount() {
+    // Remove o evento quando o componente for destruído
+    window.removeEventListener('load', this.onLoad);
+  },
+};
+
+</script>
